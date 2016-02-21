5200d987acea3cc6822fcc31e8e80af3ab79a774
Alex Jordan <alex@strugee.net>
2016-02-21 01:34:18 -0800

    Start building a basic debugger

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,61 +1,98 @@
-function initGame () {
-	var BABYLON = require('babylonjs/babylon.js');
+(function(){
+	'use strict';
 
-	console.log("Hello BabylonJS");
-	
-	// Get the canvas element from our HTML above
-	var canvas = document.querySelector("#render");
+	// If changing this make sure to make the same change in the HTML
+	var debuggerEnabled = true;
+	var debuggerToggle;
+	var debuggerToggleStateText;
+	var startButton;
 
-	// Load the BABYLON 3D engine
-	var engine = new BABYLON.Engine(canvas, true);
-	
-	// This begins the creation of a function that we will 'call' just after it's built
-	var createScene = function () {
+	// Wait for the DOM to be ready
+	document.addEventListener('DOMContentLoaded', init);
 
-		// Now create a basic Babylon Scene object 
-		var scene = new BABYLON.Scene(engine);
+	function init() {
+		// Sets up event handlers, initializes element variables, etc.
+		startButton = document.querySelector('#start');
+		debuggerToggle = document.querySelector('#debugger');
+		debuggerToggleStateText = document.querySelector('#debuggerState');
 
-		// Change the scene background color to black.
-		scene.clearColor = new BABYLON.Color3(0, 0, 0);
+		startButton.addEventListener('click', initGame);
 
-		// This creates and positions a free camera
-		var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
+		debuggerToggle.addEventListener('click', function(event) {
+			switch (debuggerToggleStateText.innerText) {
+			case 'ON':
+				debuggerToggleStateText.innerText = 'OFF';
+				debuggerEnabled = false;
+				console.log('Debugger disabled.');
+				break;
+			case 'OFF':
+				debuggerToggleStateText.innerText = 'ON';
+				debuggerEnabled = true;
+				console.log('Debugger enabled.');
+				break;
+			default:
+				throw new Error('Debugger button is in an unknown state!');
+			}
+		});
+	}
 
-		// This targets the camera to scene origin
-		camera.setTarget(BABYLON.Vector3.Zero());
+	function initGame () {
+		var BABYLON = require('babylonjs/babylon.js');
 
-		// This attaches the camera to the canvas
-		camera.attachControl(canvas, false);
+		console.log("Hello BabylonJS");
+		
+		// Get the canvas element from our HTML above
+		var canvas = document.querySelector("#render");
 
-		// This creates a light, aiming 0,1,0 - to the sky.
-		var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
+		// Load the BABYLON 3D engine
+		var engine = new BABYLON.Engine(canvas, true);
+		
+		// This begins the creation of a function that we will 'call' just after it's built
+		var createScene = function () {
 
-		// Dim the light a small amount
-		light.intensity = .5;
+			// Now create a basic Babylon Scene object 
+			var scene = new BABYLON.Scene(engine);
 
-		// Let's try our built-in 'sphere' shape. Params: name, subdivisions, size, scene
-		var sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 2, scene);
+			// Change the scene background color to black.
+			scene.clearColor = new BABYLON.Color3(0, 0, 0);
 
-		// Move the sphere upward 1/2 its height
-		sphere.position.y = 1;
+			// This creates and positions a free camera
+			var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
 
-		// Let's try our built-in 'ground' shape.  Params: name, width, depth, subdivisions, scene
-		var ground = BABYLON.Mesh.CreateGround("ground1", 6, 6, 2, scene);
+			// This targets the camera to scene origin
+			camera.setTarget(BABYLON.Vector3.Zero());
 
-		// Leave this function
-		return scene;
+			// This attaches the camera to the canvas
+			camera.attachControl(canvas, false);
 
-	};  // End of createScene function
-	
-	var scene = createScene();
-	
-	engine.runRenderLoop(function () {
-		scene.render();
-	});
-	
-	window.addEventListener("resize", function () {
-		engine.resize();
-	});
-}
+			// This creates a light, aiming 0,1,0 - to the sky.
+			var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
 
-document.addEventListener("DOMContentLoaded", initGame);
+			// Dim the light a small amount
+			light.intensity = .5;
+
+			// Let's try our built-in 'sphere' shape. Params: name, subdivisions, size, scene
+			var sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 2, scene);
+
+			// Move the sphere upward 1/2 its height
+			sphere.position.y = 1;
+
+			// Let's try our built-in 'ground' shape.  Params: name, width, depth, subdivisions, scene
+			var ground = BABYLON.Mesh.CreateGround("ground1", 6, 6, 2, scene);
+
+			// Leave this function
+			return scene;
+
+		};  // End of createScene function
+		
+		var scene = createScene();
+		
+		engine.runRenderLoop(function () {
+			scene.render();
+		});
+		
+		window.addEventListener("resize", function () {
+			engine.resize();
+		});
+	}
+})();
