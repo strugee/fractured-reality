80922bcc5fa0d40e51591e1acd3118fc58b4d882
Alex Jordan <alex@strugee.net>
2016-02-21 01:13:20 -0800

    Make gulp watch actually rebuild JS

--- gulpfile.js
+++ gulpfile.js
@@ -44,7 +44,7 @@ gulp.task('js', function () {
 gulp.task('watch', function() {
 	gulp.watch('app/*.html', ['html']);
 	gulp.watch('app/css/*.css', ['css']);
-	gulp.watch('app/*.js', ['js']);
+	gulp.watch('app/scripts/*.js', ['js']);
 	gulp.watch('app/assets/*', ['assets']);
 });
 
