1e04661cb4baa57437d789718f1ad6ff3c4dbf9e
Alex Jordan <alex@strugee.net>
2016-02-28 18:12:21 -0800

    Start adding the grid inspector view code

--- app/scripts/debugger/gridInspector.js
+++ app/scripts/debugger/gridInspector.js
@@ -1,8 +1,24 @@
 var gridInspector = {};
 
 gridInspector.init = function(view) {
-	debugger;
-	view.innerHTML = 'Inspecting the grid!';
+	var table = document.createElement('table');
+	
+	// For now, assume each biome is the same size, so that megabiomes are 12x12
+	for (var i = 0; i < 12; i++) {
+		var tr = document.createElement('tr');
+		
+		for (var j = 0; j < 12; j++) {
+			var td = document.createElement('td');
+			var text = document.createTextNode('a');
+			td.appendChild(text);
+			td.classList.add('gridInspector-cell');
+			tr.appendChild(td);
+		}
+		
+		table.appendChild(tr);
+	}
+	
+	view.appendChild(table);
 };
 
 gridInspector.teardown = function(view) {
