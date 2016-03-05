bc8593f4c5d89664d2f1261c738d74e91b0a3576
Alex Jordan <alex@strugee.net>
2016-03-04 21:02:57 -0800

    Start implementing megabiome generation

--- app/scripts/generateMegabiome.js
+++ app/scripts/generateMegabiome.js
@@ -1,7 +1,26 @@
 var gmb = {};
 
-gmb.megabiome = function() {
+var seed = require('./seed.js')();
+var Megabiome = require('./types/megabiome.js');
+var random = require('seed-random');
+var assert = require('assert');
 
+function getRandom(x, y, desc) {
+	return random(seed + '.' + x + '.' + y + desc );
+}
+
+gmb.megabiome = function(x, y) {
+	// Modulo 10 because there's a 1 in 10 chance of physics not being required
+	var hasPhysics = Math.floor(getRandom(x, y, 'physics') * 10) % 10;
+	assert(hasPhysics <= 9);
+	assert(hasPhysics >= 0);
+	hasPhysics = hasPhysics === 0 ? false : true;
+
+	var megabiome = new Megabiome(12, {
+		physics: hasPhysics
+	});
+
+	console.log('Creating new megabiome at (' + x + ', ' + y + '): ' + megabiome);
 };
 
 gmb.megabiomeType = function() {
